360183bf3628fbad6ca5bab5bef0f14a711e1191
DestiX92 <130171667+DestiX92@users.noreply.github.com>
2026-01-26 21:01:38 -0600

    Semana 1-2

---
+++ Documents/NetBeansProjects/Tienda_Ufide/src/main/resources/static/js/rutinas.js
@@ -0,0 +1,43 @@
+function addCart(formulario) {
+    var idProducto = formulario.elements[0].value;
+    var existencias = formulario.elements[1].value;
+    if (existencias>0) {
+        var ruta="/carrito/agregar/"+idProducto;
+        $ ("#resultBlock").load(ruta);
+        alert("Se va a comprar");
+    } else {
+        alert("No hay existencias");
+    }
+}
+
+
+
+
+
+function muestraImagen(input) {
+    if (input.files && input.files[0]) {
+        var lector = new FileReader();
+
+        lector.onload = function (e) {
+            $('#blah').attr('src', e.target.result)
+                    .height(200);
+        };
+
+        lector.readAsDataURL(input.files[0]);
+    }
+}
+
+//Para insertar información en el modal según el registro...
+document.addEventListener('DOMContentLoaded', function () {
+    const confirmModal = document.getElementById('confirmModal');
+    confirmModal.addEventListener('show.bs.modal', function (event) {
+        const button = event.relatedTarget;
+        document.getElementById('modalId').value = button.getAttribute('data-bs-id');
+        document.getElementById('modalDescripcion').textContent = button.getAttribute('data-bs-descripcion');
+    });
+});
+
+//Para eliminar las ventanas Toast
+    setTimeout(() => {
+        document.querySelectorAll('.toast').forEach(t => t.classList.remove('show'));
+    }, 3000);
