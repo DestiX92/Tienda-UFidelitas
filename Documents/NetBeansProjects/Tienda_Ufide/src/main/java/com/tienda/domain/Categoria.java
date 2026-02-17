224a1fb4814b5765df80e1cc3b959719048b412d
DestiX92 <130171667+DestiX92@users.noreply.github.com>
2026-02-16 19:14:01 -0600

    Semana 6.5

--- Documents/NetBeansProjects/Tienda_Ufide/src/main/java/com/tienda/domain/Categoria.java
+++ Documents/NetBeansProjects/Tienda_Ufide/src/main/java/com/tienda/domain/Categoria.java
@@ -1,10 +1,12 @@
 package com.tienda.domain;
 
+import java.util.List;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -35,6 +37,11 @@ public class Categoria implements Serializable {
 
     @Column(name = "activo")
     private Boolean activo;
+    
+    // Relación de uno a muchos con la clase Producto
+// Sin "cascade" ni "orphanRemoval" para evitar la propagación de operaciones.
+    @OneToMany(mappedBy = "categoria")
+    private List<Producto> productos;
 }
 
 //Dependencias no se registran bien, causando errores, solucion desconocida
